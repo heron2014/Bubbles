0c43e5553b8eedd7f229dbc11f1a7e2ce89348fc
Anita <a.nita@hotmail.co.uk>
2016-07-30 12:53:27 +0100

    logout

--- test/_runner.js
+++ test/_runner.js
@@ -4,4 +4,5 @@ require('./login/index.test.js');
 require('./room/index.test.js');
 require('./db/index.test.js');
 require('./fb/index.test.js');
+require('./logout/index.test.js');
 require('./end/index.test.js');
