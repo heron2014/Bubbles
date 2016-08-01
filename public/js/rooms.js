da05a7f12283513e527986b73d85c1099ada8e91
Anita <a.nita@hotmail.co.uk>
2016-08-01 11:04:24 +0100

    storing in hapi memory the list of rooms and new room

--- public/js/rooms.js
+++ public/js/rooms.js
@@ -5,8 +5,33 @@ console.log('working');
 
   $(() => {
     var socket = io('http://localhost:3000/roomslist'); //roomslist is namespace for socket - it is not a route!
+    var newRoomInput = $("input[name='newRoom']");
+
+    var renderChatRooms = chatrooms => {
+      var roomsListUL = $('#roomsListUL');
+      var listStr = '';
+      for (let cat of chatrooms) {
+        console.log(chatrooms);
+
+        listStr += `<a href="/room/${cat.title}"><li>${cat.title}</li>`;
+      }
+      roomsListUL.html('').append(listStr);
+    }
+
+    // get the list of chatrooms
+    socket.emit('getChatRooms');
+    socket.on('chatRoomsList', chatrooms => {
+      renderChatRooms(JSON.parse(chatrooms));
+    });
 
     socket.on('connect', () => console.log('Connected to the Server'));
+
+    $('#createBtn').on('click', () => {
+      if (newRoomInput !== '') {
+        socket.emit('createNewRoom', newRoomInput.val());
+        newRoomInput.val('');
+      }
+    })
   });
 
 }(window.jQuery))
